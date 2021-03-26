5fc1d7ab997888f1b5d0db16525095e4479bdde9
Sandeep Kumawat <sandeep.kumawat@taazaa.com>
2021-03-26 14:21:56 +0530

    Simple Expression Tree

---
+++ ExpressionTree/ExpressionTreeDemo/ExpressionTreeDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExpressionTreeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Func<int, int> Square = (x) => x * x;
+            //Func<int, int> Square = (x) => {return x * x;};
+            //Console.WriteLine(Square(7));
+            Func<int, int> Square = (x) => x * x;
+            System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            Console.WriteLine(e);
+            var a = e.Compile();
+            Console.WriteLine(a(7));
+
+
+
+        }
+    }
+}
